b42b7f729a2228d37f10f977ae2216f0007503a9
William Blankenship <william.jblankenship@gmail.com>
2014-04-03 04:05:09 +0000

    Updating log to identify issue with test.

--- tests/register_test.go
+++ tests/register_test.go
@@ -2,18 +2,30 @@ package tests
 
 import "testing"
 import "net/http"
+import "math/rand"
 
 func TestApp(t *testing.T) {
 	client := &http.Client{};
 
 	req,err:=http.NewRequest("GET","http://127.0.0.1:8081/test",nil)
-	if(err!=nil) {
-		t.Error("Error making request!")
+	if err!=nil {
+		t.Error("Error making request: ",err)
 	}
 	resp,err:=client.Do(req)
-	if(err!=nil) {
-		t.Error("Error executing request!")
-	} else if(resp.StatusCode!=500) {
+	if err!=nil {
+		t.Error("Error executing request: "err)
+	} else if resp.StatusCode!=500 {
 		t.Error("Received Status: ",resp.Status)
+	}
+}
+
+func TestDistribution(t *testing.T) {
+	min,max := 0,200
+	r := rand.New(rand.NewSource(20))
+	for i:=0;i<10000;i++ {
+		sample := int(r.Float64()) * (max-min) + min
+		if sample < min || sample > max {
+			t.Error(sample)
+		}
 	}
 }
