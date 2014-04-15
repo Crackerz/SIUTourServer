966e44c6dc8f34fb3272c8644093c7b87e353bde
William Blankenship <william.jblankenship@gmail.com>
2014-04-14 23:50:07 -0400

    Finished test for registering app

--- tests/register_test.go
+++ tests/register_test.go
@@ -17,20 +17,19 @@ import (
 var client = &http.Client{}
 var url = "http://127.0.0.1:80"
 
-func QueryServer(method, resource, body string) (responseBody string, responseCode int, err error) {
+func QueryServer(method, resource, body string) (responseBody []byte, responseCode int, err error) {
 	req, err := http.NewRequest(method, url+resource, strings.NewReader(body))
 	if err != nil {
-		return "", 0, errors.New("Could not create request client side.\n" + err.Error())
+		return nil, 0, errors.New("Could not create request client side.\n" + err.Error())
 	}
 
 	resp, err := client.Do(req)
 	if err != nil {
-		return "", 0, errors.New("Failed executing request.\n" + err.Error())
+		return nil, 0, errors.New("Failed executing request.\n" + err.Error())
 	}
 
 	defer resp.Body.Close()
-	rbody, err := ioutil.ReadAll(resp.Body)
-	responseBody = string(rbody)
+	responseBody, err = ioutil.ReadAll(resp.Body)
 	responseCode = resp.StatusCode
 	return
 }
@@ -59,7 +58,7 @@ func TestCreateApp(t *testing.T) {
 	}
 
 	type AppResult struct {
-		Result struct {
+		Result *struct {
 			Id int
 		}
 	}
@@ -68,7 +67,7 @@ func TestCreateApp(t *testing.T) {
 	if err != nil {
 		t.Fatal("Unable to marshal json in test:  CreateApp")
 	}
-	_, code, err := QueryServer("POST", "/app", string(body))
+	respBody, code, err := QueryServer("POST", "/app", string(body))
 
 	if err != nil {
 		t.Error("Could not make request.\n", err.Error())
@@ -78,6 +77,14 @@ func TestCreateApp(t *testing.T) {
 	}
 
 	/**TODO: check values of returned object**/
+    var respJSON AppResult
+    err = json.Unmarshal(respBody,&respJSON)
+
+    if err != nil {
+        t.Error(err.Error(),"\nUnable to parse response as JSON.\nMessage Body:\n",string(respBody))
+    } else if respJSON.Result != nil {
+        t.Error("No Result object in response.\nMessage Body:\n",string(respBody))
+    }
 }
 
 //func QueryServer(method, resource, body string) (responseBody string, responseCode int, err error)
