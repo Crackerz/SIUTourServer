017b60651dc58d8266426cd9717c5ce3be9d0920
William Blankenship <william.jblankenship@gmail.com>
2014-04-17 13:36:12 -0500

    Fixed some minor bugs with the gamification sql database

--- Gamification.sql
+++ Gamification.sql
@@ -3,12 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2014 at 07:33 PM
+-- Generation Time: Apr 17, 2014 at 08:33 PM
 -- Server version: 5.5.34
 -- PHP Version: 5.4.22
-
-CREATE DATABASE IF NOT EXISTS `gamification`;
-USE `gamification`;
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -20,8 +17,10 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `Gamification`
+-- Database: `gamification`
 --
+CREATE DATABASE IF NOT EXISTS `gamification` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `gamification`;
 
 -- --------------------------------------------------------
 
@@ -29,14 +28,29 @@ SET time_zone = "+00:00";
 -- Table structure for table `action`
 --
 
+DROP TABLE IF EXISTS `action`;
 CREATE TABLE IF NOT EXISTS `action` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `type` int(11) NOT NULL,
   `value` text NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `action_type`
+--
+
+DROP TABLE IF EXISTS `action_type`;
+CREATE TABLE IF NOT EXISTS `action_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -44,14 +58,15 @@ CREATE TABLE IF NOT EXISTS `action` (
 -- Table structure for table `app`
 --
 
+DROP TABLE IF EXISTS `app`;
 CREATE TABLE IF NOT EXISTS `app` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Unique Identifier',
   `name` text NOT NULL COMMENT 'Name of Application',
   `token` varchar(550) NOT NULL COMMENT 'Unique App Token Used For Authentication',
-  `status` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
 
 -- --------------------------------------------------------
 
@@ -59,13 +74,14 @@ CREATE TABLE IF NOT EXISTS `app` (
 -- Table structure for table `map_action_objective`
 --
 
+DROP TABLE IF EXISTS `map_action_objective`;
 CREATE TABLE IF NOT EXISTS `map_action_objective` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `action_id` int(11) NOT NULL,
   `objective_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -73,12 +89,13 @@ CREATE TABLE IF NOT EXISTS `map_action_objective` (
 -- Table structure for table `map_objective_app`
 --
 
+DROP TABLE IF EXISTS `map_objective_app`;
 CREATE TABLE IF NOT EXISTS `map_objective_app` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `objective_id` int(11) NOT NULL,
   `app_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -86,13 +103,14 @@ CREATE TABLE IF NOT EXISTS `map_objective_app` (
 -- Table structure for table `map_objective_user`
 --
 
+DROP TABLE IF EXISTS `map_objective_user`;
 CREATE TABLE IF NOT EXISTS `map_objective_user` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `objective_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -100,13 +118,14 @@ CREATE TABLE IF NOT EXISTS `map_objective_user` (
 -- Table structure for table `map_reward_objective`
 --
 
+DROP TABLE IF EXISTS `map_reward_objective`;
 CREATE TABLE IF NOT EXISTS `map_reward_objective` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `reward_id` int(11) NOT NULL,
   `objective_id` int(11) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -114,12 +133,14 @@ CREATE TABLE IF NOT EXISTS `map_reward_objective` (
 -- Table structure for table `map_user_app`
 --
 
+DROP TABLE IF EXISTS `map_user_app`;
 CREATE TABLE IF NOT EXISTS `map_user_app` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `app_id` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -127,27 +148,15 @@ CREATE TABLE IF NOT EXISTS `map_user_app` (
 -- Table structure for table `objective`
 --
 
+DROP TABLE IF EXISTS `objective`;
 CREATE TABLE IF NOT EXISTS `objective` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text NOT NULL,
-  `state` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `object_type`
---
-
-CREATE TABLE IF NOT EXISTS `object_type` (
-  `id` int(11) NOT NULL,
-  `name` text NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
 
 -- --------------------------------------------------------
 
@@ -155,14 +164,15 @@ CREATE TABLE IF NOT EXISTS `object_type` (
 -- Table structure for table `reward`
 --
 
+DROP TABLE IF EXISTS `reward`;
 CREATE TABLE IF NOT EXISTS `reward` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `type` int(11) NOT NULL,
   `value` text NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -170,12 +180,13 @@ CREATE TABLE IF NOT EXISTS `reward` (
 -- Table structure for table `reward_type`
 --
 
+DROP TABLE IF EXISTS `reward_type`;
 CREATE TABLE IF NOT EXISTS `reward_type` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -183,16 +194,17 @@ CREATE TABLE IF NOT EXISTS `reward_type` (
 -- Table structure for table `user`
 --
 
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `fname` text NOT NULL,
   `lname` text NOT NULL,
   `bday` date NOT NULL,
   `userId` text NOT NULL,
-  `state` int(11) NOT NULL,
+  `state` int(11) NOT NULL DEFAULT '0',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
