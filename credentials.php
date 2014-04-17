85e9bd7ee7aa2023c843d89fe6a7440c299d574f
William Blankenship <william.jblankenship@gmail.com>
2014-04-17 12:39:45 -0500

    Created database wrappers for all API enpoints defined

--- credentials.php
+++ credentials.php
@@ -5,4 +5,9 @@
 	$GLOBALS["SERVER_DATABASE"] = "gamification";
 	$GLOBALS["APP_TABLE"] = "app";
 	$GLOBALS["USER_TABLE"] = "user";
+	$GLOBALS["OBJECTIVE_TABLE"] = "objective";
+	$GLOBALS["ACTION_TABLE"] = "action";
+	$GLOBALS["ACTION_TYPE_TABLE"] = "action_type";
+	$GLOBALS["REWARD_TABLE"] = "reward";
+	$GLOBALS["REWARD_TYPE_TABLE"] = "reward_type";
 ?>
--- database.php
+++ database.php
@@ -1,6 +1,9 @@
 <?php
 require('./credentials.php');
 
+//This php script will be included by another, all objects passed in are defined by the calling script.
+//TODO: PREVENT INJECTION
+
 class Database {
 	public $connection;
 
@@ -18,12 +21,37 @@ class Database {
 	}
 
 	public function createApp($app) {
-		$query = "INSERT INTO `".$GLOBALS["APP_TABLE"]."` (`name`,`token`,`status`) VALUES ('".$app->Name."','".hash("sha512",rand())."',0)";
+		$query = "INSERT INTO `".$GLOBALS["APP_TABLE"]."` (`name`,`token`) VALUES ('".$app->Name."','".hash("sha512",rand())."')";
 		return $this->connection->query($query);
 	}
 	
 	public function createUser($user) {
 		$query = "INSERT INTO `".$GLOBALS["USER_TABLE"]."` (`fname`,`lname`,`bday`,`userId`) VALUES ('".$user->Fname."','".$user->Lname."','".$user->Birthday."','".$user->UserId."')";
+		return $this->connection->query($query);
+	}
+
+	public function createObjective($objective) {
+		$query = "INSERT INTO `".$GLOBALS["OBJECTIVE_TABLE"]."` (`name`,`description`) VALUES ('".$objective->Name."','".$objective->Description."')";
+		return $this->connection->query($query);
+	}
+
+	public function createAction($action) {
+		$query = "INSERT INTO `".$GLOBALS["ACTION_TABLE"]."` (`name`,`type`,`value`) VALUES ('".$action->Name."','".$action->Type."',".$action->Value.")";
+		return $this->connection->query($query);
+	}
+
+	public function createActionType($actionType) {
+		$query = "INSERT INTO `".$GLOBALS["ACTION_TYPE_TABLE"]."` (`name`, `description`) VALUES ('".$actionType->Name."','".$actionType->Description."')";
+		return $this->connection->query($query);
+	}
+
+	public function createReward($reward) {
+		$query = "INSERT INTO `".$GLOBALS["REWARD_TABLE"]."` (`name`,`type`,`value`) VALUES ('".$reward->Name."','".$reward->Type."','".$reward->Value."')";
+		return $this->connection->query($query);
+	}
+
+	public function createRewardType($rewardType) {
+		$query = "INSERT INTO `".$GLOBALS["REWARD_TYPE_TABLE"]."` (`name`,`description`) VALUES ('".$rewardType->Name."','".$rewardType->Description."')";
 		return $this->connection->query($query);
 	}
 }
