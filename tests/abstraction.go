9213a0f67a9c0bafe1125eeaf044d252b0b646c0
William Blankenship <william.jblankenship@gmail.com>
2014-04-24 01:09:13 -0400

    refactored

--- tests/abstraction.go
+++ tests/abstraction.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"errors"
 	"net/http"
-	"fmt"
 	"strings"
 	"io/ioutil"
 	"strconv"
@@ -41,30 +40,18 @@ func createApp(name string) (assetID int, err error) {
 		}
 	}
 
-	body, err := json.Marshal(AppRequest{name})
-	if err != nil {
-		return 0, errors.New("Unable to marshal json in test:  CreateApp\n"+err.Error())
+    var input = AppRequest{name}
+    var output AppResult
+    respBody,err := makeRequest(input,&output, "POST", "/app")
+
+    if err != nil {
+        return 0, errors.New("Message Body: "+string(respBody)+"\n"+err.Error());
+    }
+    if output.Result == nil {
+        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
 	}
 
-	respBody, code, err := QueryServer("POST", "/app", string(body))
-
-	if err != nil {
-		return 0, errors.New("Could not make request.\n"+err.Error())
-	}
-	if code != 201 {
-		return 0, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
-	}
-
-	var respJSON AppResult
-	err = json.Unmarshal(respBody,&respJSON)
-
-	if err != nil {
-		return 0, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
-	} else if respJSON.Result == nil {
-		return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
-	}
-
-	return respJSON.Result.Id, nil
+	return output.Result.Id, nil;
 }
 
 func createUser(fname, lname, birthday, userID string) (assetID int, err error) {
@@ -81,29 +68,18 @@ func createUser(fname, lname, birthday, userID string) (assetID int, err error) 
 		}
     }
 
-    body,err := json.Marshal(UserRequest{fname,lname,birthday,userID})
-	if err != nil {
-		return 0, errors.New("Could not marshal json in Test: createUser.\n"+err.Error())
-	}
-
-	respBody, code, err := QueryServer("POST", "/user", string(body))
-	if err != nil {
-		return 0, errors.New("Could not make request.\n"+err.Error())
-	}
-	if code != 201 {
-		return 0, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
-	}
-
-    var respJSON UserResult
-    err = json.Unmarshal(respBody,&respJSON)
+    var input = UserRequest{fname,lname,birthday,userID}
+    var output UserResult
+    respBody,err := makeRequest(input,&output, "POST", "/user")
 
     if err != nil {
-        return 0, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
-    } else if respJSON.Result == nil {
-        return 0, errors.New(err.Error()+"No Result object in response.\nMessage Body:\n"+string(respBody))
+        return 0, errors.New("Message Body: "+string(respBody)+"\n"+err.Error());
     }
+    if output.Result == nil {
+        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+	}
 
-    return respJSON.Result.Id, nil
+	return output.Result.Id, nil;
 }
 
 func createAction(name, description string, actionType int) (assetID int, err error) {
@@ -119,33 +95,21 @@ func createAction(name, description string, actionType int) (assetID int, err er
 		}
     }
 
-    body,err := json.Marshal(ActionRequest{name,actionType,description})
-	if err != nil {
-		return 0, errors.New("Could not marshal JSON in test: CreateAction.\n"+err.Error())
-	}
-
-	respBody, code, err := QueryServer("POST", "/action", string(body))
-	if err != nil {
-		return 0, errors.New("Could not make request.\n"+err.Error())
-	}
-	if code != 201 {
-		return 0, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
-	}
-
-    var respJSON ActionResult
-    err = json.Unmarshal(respBody,&respJSON)
+    var input = ActionRequest{name,actionType,description}
+    var output ActionResult
+    respBody,err := makeRequest(input,&output, "POST", "/action")
 
     if err != nil {
-        return 0, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
-    } else if respJSON.Result == nil {
-        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+        return 0, errors.New("Message Body: "+string(respBody)+"\n"+err.Error());
     }
+    if output.Result == nil {
+        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+	}
 
-    return respJSON.Result.Id, nil
+	return output.Result.Id, nil;
 }
 
 func createObjective(name,description string) (assetID int, err error) {
-
     type ObjectiveRequest struct {
         Name string
         Description string
@@ -157,29 +121,18 @@ func createObjective(name,description string) (assetID int, err error) {
 		}
     }
 
-    body,err := json.Marshal(ObjectiveRequest{name,description})
-	if err != nil {
-		return 0, errors.New("Could not marshal JSON in test: CreateObjective.\n"+err.Error())
-	}
-
-	respBody, code, err := QueryServer("POST", "/objective", string(body))
-	if err != nil {
-		return 0, errors.New("Could not make request.\n"+err.Error())
-	}
-	if code != 201 {
-		return 0, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
-	}
-
-    var respJSON ObjectiveResult
-    err = json.Unmarshal(respBody,&respJSON)
+    var input = ObjectiveRequest{name,description}
+    var output ObjectiveResult
+    respBody,err := makeRequest(input,&output, "POST", "/objective")
 
     if err != nil {
-        return 0, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
-    } else if respJSON.Result == nil {
-        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+        return 0, errors.New("Message Body: "+string(respBody)+"\n"+err.Error());
     }
+    if output.Result == nil {
+        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+	}
 
-    return respJSON.Result.Id, nil
+	return output.Result.Id, nil;
 }
 
 func createReward(name, value string, rewardType int) (assetId int, err error) {
@@ -195,29 +148,18 @@ func createReward(name, value string, rewardType int) (assetId int, err error) {
 		}
     }
 
-    body,err := json.Marshal(RewardRequest{name,rewardType,value})
-	if err != nil {
-		return 0, errors.New("Could not marshal JSON in test: CreateReward.\n"+err.Error())
-	}
-
-	respBody, code, err := QueryServer("POST", "/reward", string(body))
-	if err != nil {
-		return 0, errors.New("Could not make request.\n"+err.Error())
-	}
-	if code != 201 {
-		return 0, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
-	}
-
-    var respJSON RewardResult
-    err = json.Unmarshal(respBody,&respJSON)
+    var input = RewardRequest{name,rewardType,value}
+    var output RewardResult
+    respBody,err := makeRequest(input,&output, "POST", "/reward")
 
     if err != nil {
-        return 0, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
-    } else if respJSON.Result == nil {
-        return 0 , errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+        return 0, errors.New("Message Body: "+string(respBody)+"\n"+err.Error());
     }
+    if output.Result == nil {
+        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+	}
 
-    return respJSON.Result.Id, nil;
+	return output.Result.Id, nil;
 }
 
 func createActionType(name, description string) (assetId int, err error) {
@@ -232,29 +174,18 @@ func createActionType(name, description string) (assetId int, err error) {
 		}
     }
 
-    body,err := json.Marshal(ActionTypeRequest{name,description})
-	if err != nil {
-		return 0, errors.New("Could not marshal JSON in test: CreateActionType.\n"+ err.Error())
-	}
-
-	respBody, code, err := QueryServer("POST", "/type/action", string(body))
-	if err != nil {
-		return 0, errors.New("Could not make request.\n"+ err.Error())
-	}
-	if code != 201 {
-		return 0, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
-	}
-
-    var respJSON ActionTypeResult
-    err = json.Unmarshal(respBody,&respJSON)
+    var input = ActionTypeRequest{name,description}
+    var output ActionTypeResult
+    respBody,err := makeRequest(input,&output, "POST", "/type/action")
 
     if err != nil {
-        return 0, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
-    } else if respJSON.Result == nil {
-        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+        return 0, errors.New("Message Body: "+string(respBody)+"\n"+err.Error());
     }
+    if output.Result == nil {
+        return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
+	}
 
-	return respJSON.Result.Id, nil;
+	return output.Result.Id, nil;
 }
 
 func createRewardType(name, description string) (assetId int, err error) {
@@ -269,34 +200,38 @@ func createRewardType(name, description string) (assetId int, err error) {
 		}
     }
 
-    body,err := json.Marshal(RewardTypeRequest{name,description})
-	if err != nil {
-		return 0, errors.New("Could not marshal JSON in test: CreateActionType.\n"+ err.Error())
-	}
-
-	respBody, code, err := QueryServer("POST", "/type/action", string(body))
-	if err != nil {
-		return 0, errors.New("Could not make request.\n"+ err.Error())
-	}
-	if code != 201 {
-		return 0, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
-	}
-
-    var respJSON RewardTypeResult
-    err = json.Unmarshal(respBody,&respJSON)
+    var input = RewardTypeRequest{name,description}
+    var output RewardTypeResult
+    respBody,err := makeRequest(input,&output, "POST", "/type/reward")
 
     if err != nil {
-        return 0, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
-    } else if respJSON.Result == nil {
+        return 0, errors.New("Message Body: "+string(respBody)+"\n"+err.Error());
+    }
+    if output.Result == nil {
         return 0, errors.New("No Result object in response.\nMessage Body:\n"+string(respBody))
     }
 
-	return respJSON.Result.Id, nil;
+	return output.Result.Id, nil;
 }
-func main() {
-	id,err:=createApp("Testing");
-	if err!=nil {
-		fmt.Println(err.Error());
+
+func makeRequest(input interface{}, output interface{}, method, url string) ([]byte, error) {
+    body,err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.New("Could not marshal JSON in test: CreateActionType.\n"+ err.Error())
 	}
-	fmt.Println(strconv.Itoa(id));
+
+	respBody, code, err := QueryServer(method, url, string(body))
+	if err != nil {
+		return respBody, errors.New("Could not make request.\n"+ err.Error())
+	}
+	if code != 201 {
+		return respBody, errors.New("Received incorrect error code. Expected 201 and recieved "+strconv.Itoa(code))
+	}
+
+    err = json.Unmarshal(respBody,&output)
+
+    if err != nil {
+        return respBody, errors.New(err.Error()+"\nUnable to parse response as JSON.\nMessage Body:\n"+string(respBody))
+    }
+    return respBody, nil;
 }
